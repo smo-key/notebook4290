38ce276b6435a7a4a90c166c30f2f8ed5a90164b
Ehsan Asdar <ehsanmasdar@gmail.com>
2014-09-17 19:49:55 -0400

    Fix image upload

--- upload.php
+++ upload.php
@@ -14,14 +14,14 @@ if ((($_FILES["file"]["type"] == "image/gif")
     echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
   } else {
     echo "Uploading: " . $_FILES["file"]["name"] . "<br>";
-    if (file_exists("images/" . $_FILES["file"]["name"])) {
+    if (file_exists("img/" . $_FILES["file"]["name"])) {
       echo $_FILES["file"]["name"] . " already exists. ";
     } else {
       move_uploaded_file($_FILES["file"]["tmp_name"],
-      "images/" . $_FILES["file"]["name"]);
+      "img/" . $_FILES["file"]["name"]);
       echo "Upload Sucessful! Paste the below code into your blog entry: PLACEHOLDER!!!!!!!!!!!!!!!!!!!!!!! <br>";
-      echo '<a href="' , 'http://ehsandev.com/notebook/' , 'images/' , $_FILES["file"]["name"] , '">',
-      'http://ehsandev.com/notebook/' , 'images/' , $_FILES["file"]["name"] , '</a>';
+      echo '<a href="' , 'http://ehsandev.com/notebook/' , 'img/' , $_FILES["file"]["name"] , '">',
+      'http://ehsandev.com/notebook/' , 'img/' , $_FILES["file"]["name"] , '</a>';
     }
   }
 } else {
