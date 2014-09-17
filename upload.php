540a6c39247b9a132f79e94c23daedab060472e0
Ehsan Asdar <ehsanmasdar@gmail.com>
2014-09-16 20:37:50 -0400

    Add upload files to git

---
+++ upload.php
@@ -0,0 +1,29 @@
+<?php
+$allowedExts = array("gif", "jpeg", "jpg", "png");
+$temp = explode(".", $_FILES["file"]["name"]);
+$extension = end($temp);
+if ((($_FILES["file"]["type"] == "image/gif")
+|| ($_FILES["file"]["type"] == "image/jpeg")
+|| ($_FILES["file"]["type"] == "image/jpg")
+|| ($_FILES["file"]["type"] == "image/pjpeg")
+|| ($_FILES["file"]["type"] == "image/x-png")
+|| ($_FILES["file"]["type"] == "image/png"))
+&& ($_FILES["file"]["size"] < 5*1000000)
+&& in_array($extension, $allowedExts)) {
+  if ($_FILES["file"]["error"] > 0) {
+    echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
+  } else {
+    echo "Uploading: " . $_FILES["file"]["name"] . "<br>";
+    if (file_exists("images/" . $_FILES["file"]["name"])) {
+      echo $_FILES["file"]["name"] . " already exists. ";
+    } else {
+      move_uploaded_file($_FILES["file"]["tmp_name"],
+      "images/" . $_FILES["file"]["name"]);
+      echo "Upload Sucessful! Paste the below code into your blog entry: <br>";
+      echo '<a href="' , 'http://ehsandev.com/notebook/' , 'images/' , $_FILES["file"]["name"] , '">',
+      'http://ehsandev.com/notebook/' , 'images/' , $_FILES["file"]["name"] , '</a>';
+    }
+  }
+} else {
+  echo "Invalid file";
+}
